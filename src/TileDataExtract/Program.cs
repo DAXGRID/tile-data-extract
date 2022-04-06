28188cad9814a05add59f2cfff51ef05da150734
Rune Nielsen <runenielsen@runbox.com>
2022-04-06 13:33:53 +0200

    adds csharp project setup

---
+++ src/TileDataExtract/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace TileDataExtract;
+
+internal static class Program
+{
+    internal static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ test/TileDataExtract.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace TileDataExtract.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
