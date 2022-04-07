4218adeee34b1e8bb3482ccd558c04305e701742
Rune Nielsen <runenielsen@runbox.com>
2022-04-07 21:18:11 +0200

    started implementation of reading values

--- src/TileDataExtract/PostgresReader.cs
+++ src/TileDataExtract/PostgresReader.cs
@@ -4,18 +4,32 @@ namespace TileDataExtract;
 
 internal class PostgresReader
 {
-    public static async IAsyncEnumerable<string> ReadTable(
+    public static async IAsyncEnumerable<string> ReadTableColumnsAsync(
         string connectionString, string query)
     {
         using var conn = new NpgsqlConnection(connectionString);
         await conn.OpenAsync().ConfigureAwait(false);
 
         using var cmd = new NpgsqlCommand(query, conn);
-
         var reader = await cmd.ExecuteReaderAsync().ConfigureAwait(false);
 
         while (await reader.ReadAsync().ConfigureAwait(false))
         {
+            var column = new Dictionary<string, object>();
+
+            for (var i = 0; i < reader.FieldCount; i++)
+            {
+                if (!reader.GetDataTypeName(i)
+                    .Contains("geometry", StringComparison.OrdinalIgnoreCase))
+                {
+                    column.Add(reader.GetName(i), reader.GetValue(i));
+                }
+                else
+                {
+                    // Do nothing right now
+                }
+            }
+
             yield return reader.GetGuid(0).ToString();
         }
     }
--- test/TileDataExtract.Tests/PostgresReaderTests.cs
+++ test/TileDataExtract.Tests/PostgresReaderTests.cs
@@ -1,5 +1,7 @@
 using FluentAssertions;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -8,11 +10,16 @@ namespace TileDataExtract.Tests;
 [Trait("Category", "Integration")]
 public class PostgresReaderTests : IClassFixture<PostgisTestFixture>
 {
-    [Fact]
-    public async Task Read_table()
+    [Theory]
+    [JsonFileData("Data/route_node.json")]
+    public async Task Read_table(string json)
     {
+        var expected = JsonSerializer.Deserialize<List<Dictionary<string, object>>>(json);
+
         var sql = "select * from route_network.route_node";
-        var count = (await PostgresReader.ReadTable(PostgisTestFixture.ConnectionString, sql).ToListAsync()).Count;
-        count.Should().Be(10);
+        var column = await PostgresReader.ReadTableColumnsAsync(
+                         PostgisTestFixture.ConnectionString, sql).ToListAsync();
+
+        column.Should().BeEquivalentTo(expected);
     }
 }
